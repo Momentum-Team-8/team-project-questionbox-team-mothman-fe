8950d438351b2338be039693e6ccd840d4792500
loganatkinson <logan.atkinson@gmail.com>
2021-07-22 16:44:56 -0400

    added basic question detail set to show when expanded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Header } from './components/Header.js'
 import { QuestionList } from './components/QuestionList.js'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare, faArrowAltCircleDown, faArrowAltCircleUp } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCheckSquare, faArrowAltCircleDown, faArrowAltCircleUp)
 
 export const App = () => {
   return (
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react'
+import { QuestionDetail } from './QuestionDetail'
 
 export const Question = (props) => {
-  const { question } = props
+  const [expand, setExpand] = useState(false)
+  const { title, author, created, body } = props
+
+  const handleExpand = () => {
+    setExpand(!expand)
+  }
 
   return (
     <div>
-      <h2>{question}</h2>
-      <button>See More</button>
+      <h2>{title}</h2>
+      <button onClick={handleExpand}>
+        {expand
+          ? <FontAwesomeIcon icon='arrow-alt-circle-up' />
+          : <FontAwesomeIcon icon='arrow-alt-circle-down' />}
+      </button>
+      {expand && (
+        <div>
+          <QuestionDetail
+            title={title}
+            author={author}
+            created={created}
+            body={body}
+          />
+        </div>
+      )}
     </div>
   )
 }
