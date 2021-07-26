2bd5582af7bf4c00777be5efbffb4ed94314723b
loganatkinson <logan.atkinson@gmail.com>
2021-07-26 13:16:36 -0400

    added framework to map out answers but stupid question detail is being a pain in the ass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import { Header } from './components/Header.js'
 import { QuestionList } from './components/QuestionList.js'
+import { QuestionDetail } from './components/QuestionDetail.js'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckSquare, faArrowAltCircleDown, faArrowAltCircleUp, faHeart, faHeartBroken } from '@fortawesome/free-solid-svg-icons'
 import { SideNav } from './components/SideNav.js'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 library.add(faCheckSquare, faArrowAltCircleDown, faArrowAltCircleUp, faHeart, faHeartBroken)
 
-export const App = () => {
+export const App = (props) => {
+  const { selectedQuestionId } = props
   return (
-    <div>
-      <Header />
-      <div className='container'>
-        <div className='sideNav'>
-          <SideNav />
-        </div>
-        <div className='body'>
-          <QuestionList />
+    <Router>
+      <div>
+        <Header />
+        <div className='container'>
+          <div className='sideNav'>
+            <SideNav />
+          </div>
+          <div className='body'>
+            <Switch>
+              <Route path='/' exact component={() => <QuestionList />} />
+              <Route path='/questions/:id' component={() => <QuestionDetail selectedQuestionId={selectedQuestionId} />} />
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   )
 }
--- src/api.js
+++ src/api.js
@@ -1,84 +1,17 @@
-const questions = [
-  {
-    id: 1,
-    title: 'Medical Advice',
-    body: 'Does anyone know if there are warning signs for a stroke?',
-    author: 'Edith Wilson',
-    created_at: '07-21-2021',
-    answers: [
-      {
-        date: '07-21-2021',
-        body: 'If you smell burnt toast you might be in trouble',
-        author: 'Abigail Fillmore',
-        accepted: true,
-        Votes: 4
-      }
-    ],
-    Favorited_by: [
-      {
-        user: 'mjefferson'
-      },
-      {
-        user: 'dmadison'
-      }
-    ]
-  },
-  {
-    id: 2,
-    title: 'Play Recommendations',
-    body: 'Are there any exciting new shows the hubby and I should check out for date night?',
-    author: 'Mary Lincoln',
-    created_at: '04-14-1865',
-    answers: [
-      {
-        date: '07-21-2021',
-        body: 'Our American Cousin has been getting great reviews...',
-        author: 'Izola Mills',
-        accepted: true,
-        Votes: -14
-      }
-    ],
-    Favorited_by: [
-      {
-        user: 'jwbooth'
-      }
-    ]
-  },
-  {
-    id: 3,
-    title: 'Low-Key Happy Hours',
-    body: "I'm looking for local bars or restaurants that tend to be a little more private, preferrably around Foggy Bottom or Dupont Circle",
-    author: 'Elizabeth Ford',
-    created_at: '05-11-1975',
-    answers: [
-      {
-        date: '05-14-1975',
-        body: 'Old Ebbitt has large booths and is pretty dimly lit',
-        author: 'Mamie Eisenhower',
-        accepted: true,
-        Votes: 7
-      },
-      {
-        date: '05-17-1975',
-        body: "I'd steer clear of Top of The Gate.  It is crawling with annoying interns and media types.",
-        author: 'Pat Nixon',
-        accepted: true,
-        Votes: -2
-      }
-    ],
-    Favorited_by: [
-      {
-        user: 'bwoodward'
-      },
-      {
-        user: 'cbernstein'
-      }
-    ]
-  }
-]
+import axios from 'axios'
 
 export const getQuestions = () => {
-  return new Promise((resolve, reject) => {
-    resolve({ questions: questions })
-  })
+  return (
+    axios
+      .get('https://questionbox-sasmothbe.herokuapp.com/api/questions/')
+      .then(res => res.data)
+  )
+}
+
+export const getQuestionDetail = (id) => {
+  return (
+    axios
+      .get(`https://questionbox-sasmothbe.herokuapp.com/api/questions/${id}/`)
+      .then(res => res.data)
+  )
 }
