7bf44cb8f51fadc10b65abe704c3b7bd1f8ac2b8
bhall20 <brianhall401@yahoo.com>
2021-07-27 16:41:51 -0400

    registration complete

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,25 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 
 export const Registration = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [name, setName] = useState('')
+  const url = 'https://questionbox-sasmothbe.herokuapp.com/api/accounts/signup'
+  const history = useHistory()
+  const handleSubmit = () => {
+    axios.post(url, {
+      name: name,
+      email: email,
+      password: password,
+      password2: confirmPassword
+    }).then(response => {
+      console.log(response)
+      history.push('/')
+    })
+  }
   return (
     <div>
       <div class='field'>
         <label class='label'>Name</label>
         <div class='control'>
-          <input class='input' type='text' placeholder='Name input' />
+          <input class='input' type='text' placeholder='Name input' value={name} onChange={event => setName(event.target.value)} />
           <i class='fas fa-id-badge' />
         </div>
       </div>
 
       <div class='field'>
-        <label class='label'>Username</label>
+        <label class='label'>Email</label>
         <div class='control has-icons-left has-icons-right'>
           <input
             class='input'
             type='text'
-            placeholder='Username input'
-            value=''
+            placeholder='email input'
+            value={email} onChange={event => setEmail(event.target.value)}
           />
           <span class='icon is-small is-left'>
             <i class='fas fa-user-tag' />
@@ -29,23 +47,30 @@ export const Registration = () => {
       </div>
 
       <div class='field'>
-        <label class='label'>Email</label>
+        <label class='label'>Password</label>
         <div class='control has-icons-left has-icons-right'>
           <input
             class='input'
-            type='email'
-            placeholder='Email input'
-            value=''
+            type='password'
+            placeholder='password input'
+            value={password} onChange={event => setPassword(event.target.value)}
           />
-          <span class='icon is-small is-left'>
-            <i class='fas fa-envelope' />
-          </span>
-          <span class='icon is-small is-right' />
+        </div>
+      </div>
+      <div class='field'>
+        <label class='label'>Confirm Password</label>
+        <div class='control has-icons-left has-icons-right'>
+          <input
+            class='input'
+            type='password'
+            placeholder='password input'
+            value={confirmPassword} onChange={event => setConfirmPassword(event.target.value)}
+          />
         </div>
       </div>
       <div class='field is-grouped'>
         <div class='control'>
-          <button class='button is-link'>Submit</button>
+          <button class='button is-link' onClick={handleSubmit}>Submit</button>
         </div>
         <div class='control'>
           <Link to='/'>
