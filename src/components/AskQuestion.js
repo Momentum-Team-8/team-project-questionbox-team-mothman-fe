42b2bb81f32405f4e9b768ef3d31f3c5b090c06d
loganatkinson <logan.atkinson@gmail.com>
2021-07-27 13:13:24 -0400

    not totally working but sort of mapped out

---
+++ src/components/AskQuestion.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+export const QuestionForm = () => {
+  const [question, setQuestion] = useState('')
+  const handleChange = (event) => {
+    setQuestion(event.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <h1>What's your Question</h1>
+        <label>
+          Type Question Title:
+          <input type='text' value={question} onChange={handleChange} />
+        </label>
+        Type Question Body: <textarea>Hello there, this is some text in a text area</textarea>
+        <p>{question}</p>
+        <input type='submit' value='Submit' />
+      </form>
+    </div>
+  )
+}
--- src/components/QuestionDetail.js
+++ src/components/QuestionDetail.js
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import { getQuestionDetail } from '../api'
+import { FavButton } from './FavButton'
+import { Vote } from './Vote'
 import '../App.css'
 
 export const QuestionDetail = (props) => {
-  const [questionDetail, setQuestionDetail] = useState({})
+  const [questionDetail, setQuestionDetail] = useState([])
   const { selectedQuestionId } = props
   useEffect(() => {
     getQuestionDetail(selectedQuestionId).then((data) => {
       setQuestionDetail(data)
+      console.log(data)
     })
   }, [selectedQuestionId])
 
   return (
-    <div key={questionDetail.id}>
-      <h2>{questionDetail.title}</h2>
-      <p>asked by:{questionDetail.author} on {questionDetail.created_at}</p>
-      <p>{questionDetail.body}</p>
-      <p> Favorited by {questionDetail.favorited_by} users</p>
-      <button>Favorite</button>
-    </div>
+    <>
+      <div className='qCard' key={questionDetail.id}>
+        <h2>{questionDetail.title}</h2>
+        <p>asked by:{questionDetail.author} on {questionDetail.created_at}</p>
+        <p>{questionDetail.body}</p>
+        <p> Favorited by {questionDetail.favorited_by} users</p>
+        <FavButton />
+      </div>
+
+      <p>Answer List Here</p>
+    </>
   )
 }
