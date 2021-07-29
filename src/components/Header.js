ceb385241d048eed7d45ba833558cfe163bd2691
loganatkinson <logan.atkinson@gmail.com>
2021-07-29 11:33:14 -0400

    fixed conditional rendering in header for login/logout/signup

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import '../App.css'
 import { Link } from 'react-router-dom'
 
 export const Header = (props) => {
-  const { isLoggedIn } = props
+  const { token } = props
   return (
     <div className='nav'>
       <Link to='/'>
@@ -12,14 +12,14 @@ export const Header = (props) => {
       <label for='searchbox'>Search QuestionBox:</label>
       <input type='search' id='searchbox' name='searchbox' />
       <br />
-      {isLoggedIn
+      {token
         ? <>
           <Link to='/logout'><p>Log out</p></Link>
-        </>
+          </>
         : <>
           <Link to='/login'><p>Login</p></Link>
           <Link to='/registration'><p>Sign Up</p></Link>
-        </>}
+          </>}
     </div>
   )
 }
